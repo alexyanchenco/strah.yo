cc8d9f31fea07110d7a84e2c47c6c779b0ec8ed9
alexyanchenco <105377305+alexyanchenco@users.noreply.github.com>
2023-04-25 17:07:25 +0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.dropdown-item').forEach(function(listItem){
-    listItem.addEventListener('click',function(){
-    document.querySelector('.dro').innerText = this.innerText;
-    })
-    })
-    
